b21787ff8ab427d39038ff5b760cca7669e87bff
unknown <nmukhtar2001@gmail.com>
2023-02-22 22:40:37 -0800

    did some js work

---
+++ Develop/index.js
@@ -0,0 +1,50 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { right } = require('inquirer/lib/utils/readline');
+const generateMarkdown = require("./utils/generateMarkdown")
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'input',
+      message: 'What is your Description?',
+      name: 'Description',
+    },
+    {
+      type: 'input',
+      message: 'what is your title :',
+      name: 'title',
+    },
+  ]
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data, (err) =>
+    err ? console.log(err) : console.log('Successfully created index.html!'))
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+     .prompt(questions)
+     .then((answers) => {
+
+     const Markdown = generateMarkdown(answers)
+
+     writeToFile ("readme.md",Markdown )
+    
+
+
+
+
+
+
+     })
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/readme.md
@@ -0,0 +1,4 @@
+# gg
+  nick
+  pizza
+
